17774731ad8b1d3bd573613ee7e1b0519b4d858b
AnthonyH <anthony.hoyas@gmail.com>
2021-11-29 22:03:39 +0100

    Major update soundcloud & graph & index for them

--- MyCharts/firstChart.js
+++ MyCharts/firstChart.js
@@ -2,11 +2,12 @@ let myChart = document.getElementById('myChart').getContext('2d');
 let labels = [];
 let labelsPop = [];
 let test = ['Bruxelles', 'Mons', 'Liège', 'Namur', 'Arlon', 'Anvers'];
+let testpop = ['1000000', '200000', '154875', '559321', '214587', '65983']
 let citiesList = fetch("be.json")
     .then(response => response.json())
     .then(cities => {
         for (let city of cities) {
-            labels.push(city.city);
+            labels.push(city.city)
         }
     })
 let poplist = fetch("be.json")
@@ -17,18 +18,17 @@ let poplist = fetch("be.json")
         }
     })
 console.log(labelsPop)
-console.log(labels);
+console.log(labels)
 console.log(test)
-
 //Global Options
 
 let belgPopChart = new Chart(myChart, {
     type: 'bar', //bar, horizontalBar, pie, line, doughnut, radar, polarArea
     data: {
-        labels: [],
+        labels: test,
         datasets: [{
             label: 'population',
-            data: labelsPop,
+            data: testpop,
             backgroundColor: [
                 'green',
                 'blue',
