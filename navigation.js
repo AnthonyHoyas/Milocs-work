4f2c1cae9b8c721a950fc6e25d1e77fa25c95708
AnthonyH <anthony.hoyas@gmail.com>
2021-11-30 15:50:05 +0100

    Update on svg navigation

--- navigation.js
+++ navigation.js
@@ -1,7 +1,7 @@
 
 
 
-document.getElementById("svg-menu").addEventListener("click",function() {
+/*document.getElementById("svg-menu").addEventListener("click",function() {
   
     if(document.getElementById("popnavbar").style.display == "none"){
       document.getElementById("popnavbar").style.display = "flex"
@@ -18,9 +18,28 @@ document.getElementById("svg-menu").addEventListener("click",function() {
       document.getElementById("r3").style.fill = "black"
 
     }
-}) 
+})*/
+
 window.addEventListener('keydown', function(event)  {
   if (event.key === 'Escape') {
     document.getElementById("popnavbar").style.display = "none"
   }
+})
+
+
+let clickNumbers = 0 ;
+let svg = document.getElementById('svg-menu');
+let r1 = document.getElementById('r1');
+let r2 = document.getElementById('r2');
+let r3 = document.getElementById('r3');
+let popnavbar = document.getElementById("popnavbar")
+
+svg.addEventListener('click', function(){
+  clickNumbers++;
+  if(clickNumbers % 2 == 1){
+    r1.style.fill = '#2a9d8f', r2.style.fill = '#2a9d8f', r3.style.fill = '#2a9d8f', popnavbar.style.display = "flex"
+  } else {
+    r1.style.fill = 'black',r2.style.fill = 'black',r3.style.fill = 'black', popnavbar.style.display = "none"
+  }
+
 })
